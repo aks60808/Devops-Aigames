a09f843b7645338808f309499e3e505f42acded4
Tommy Lin <aks60808@gmail.com>
2023-02-24 21:04:04 -0800

    comment out second worker

--- worker.tf
+++ worker.tf
@@ -18,3 +18,24 @@ resource "google_compute_instance" "deployment_server" {
     google_compute_network.vpc_network
   ]
 }
+
+# resource "google_compute_instance" "deployment_server_2" {
+#   name         = "aigames-deployment-server-2"
+#   machine_type = "e2-medium"
+#   allow_stopping_for_update = true
+#   tags         = ["http-jenkins-server", "ssh","nodejs"]
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-11"
+#     }
+#   }
+
+#   network_interface {
+#     network = "${var.project_id}-network"
+#     access_config {
+#     }
+#   }
+#   depends_on = [
+#     google_compute_network.vpc_network
+#   ]
+# }
